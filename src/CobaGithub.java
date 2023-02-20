886c6f21c0b8c0cdaa9ed3612855c176517bbffd
Samuel Putra <samueldianpermanaputra@gmail.com>
2023-02-20 23:23:42 +0100

    Ini adalah commit ketiga: nambah print "Percobaan ke github" kedua kalinya

--- src/CobaGithub.java
+++ src/CobaGithub.java
@@ -1,5 +1,6 @@
 public class CobaGithub {
     public static void main(String[] args) {
         System.out.println("Percobaan kedua untuk upload ke github");
+        System.out.println("Percobaan ketiga untuk upload ke github");
     }
 }
